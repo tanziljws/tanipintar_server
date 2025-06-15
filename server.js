0b06274b6b538b0f08cad1b4e7b44829443c3f7f
ADARIYA0 <adrianianimportant@gmail.com>
2025-06-15 08:36:26 +0700

    koneksi database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+const app = require('./app')
+const db = require('./config/db')
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 })
